8b66ee39dbd2634569ee4568685ec7b599da1b8f
Anuj Kutal <anujkutal2004@gmail.com>
2025-10-16 09:20:44 +0530

    project setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log/slog"
+	"os"
+)
+
+type config struct {
+	port int
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
 
 func main() {
-	fmt.Println("hello, world")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	err := app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 }
---
+++ cmd/api/status.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) statusHandler(w http.ResponseWriter, r *http.Request) {
+	env := envelope{
+		"status": "available",
+	}
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
